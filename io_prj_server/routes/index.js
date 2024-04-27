eb28095e73c666f77eba5db53aeddefd77f81866
jkp_06 <jan.kapusta111@gmail.com>
2024-04-27 22:09:04 +0200

    added home page template

--- io_prj_server/routes/index.js
+++ io_prj_server/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('guest');
 });
+
+router.get('/logged', function(req, res, next) {
+  res.render('logged'); 
+});
+
 
 module.exports = router;
