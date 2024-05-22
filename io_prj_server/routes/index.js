9dc939258b60019d8941db2b762539762b232e15
amlynczak <95677220+amlynczak@users.noreply.github.com>
2024-05-22 22:15:44 +0200

    Update index.js

--- io_prj_server/routes/index.js
+++ io_prj_server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const userRoutes = require('./user');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -21,5 +22,7 @@ router.get('/verify', function (req, res, next) {
 router.get('/about', function (req, res, next) {
   res.render('about');
 });
+
+router.use('/user', userRoutes);
 
 module.exports = router;
