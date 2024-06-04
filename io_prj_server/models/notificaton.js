9c544fcc2dacdb1527ff0d2b1ec8708c6fbe3f14
Leon Ożóg <leonozog221@gmail.com>
2024-06-04 21:48:24 +0200

    user notification more data

--- io_prj_server/models/notificaton.js
+++ io_prj_server/models/notificaton.js
@@ -1,12 +1,29 @@
 var db = require('@utility/database');
 
 exports.get_user_notifications = async function (user_id) {
-    return await db.request()
+    var seen_time = new Date().toISOString();
+    var notifications = await db.request()
         .input('user_id', user_id)
-        .query(`SELECT * FROM [dbo].[notification] JOIN [dbo].[notification_type] ON notification.notification_type_id = notification_type.notification_type_id
-                WHERE user_id = @user_id 
-                ORDER BY create_date DESC`)
+        .query(`SELECT  n.notification_id, n.create_date, n.seen, 
+                        t.name AS notification_type_name,
+                        u.user_id AS from_user_id, u.name AS from_user_name, u.from_user_is_admin, 
+                        p.project_id AS from_project_id, p.title AS from_project_title,
+                        n.message
+                FROM [dbo].[notification] n
+                JOIN [dbo].[notification_type] t ON n.notification_type_id = t.notification_type_id
+                JOIN [dbo].[user] u ON n.from_user_id = u.user_id
+                JOIN [dbo].[project] p ON n.from_project_id = p.project_id
+                WHERE n.user_id = @user_id 
+                ORDER BY n.create_date DESC`)
         .then(result => {
             return result.recordset;
         });
+    await db.request()
+        .input('user_id', user_id)
+        .input('seen_time', seen_time)
+        .query(`UPDATE [dbo].[notification]
+                SET seen = 1
+                WHERE user_id = @user_id
+                AND create_date <= @seen_time`);
+    return notifications;
 }
