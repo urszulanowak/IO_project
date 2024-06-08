5a61c46f90d2cc726edacc779046f125ca129fa5
Leon Ożóg <leonozog221@gmail.com>
2024-06-08 23:42:17 +0200

    update tag names in insert_tag.sql

--- random_sql/insert_tag.sql
+++ random_sql/insert_tag.sql
@@ -11,7 +11,7 @@ insert into [dbo].[tag_category](name) Values ('język programowania'), ('techno
 insert into [dbo].[tag](name, tag_category_id) Values 
 ('C', 1), ('C++', 1), ('Java', 1), ('Rust', 1), ('JavaScript', 1), ('Lua', 1), ('Python', 1), ('PHP', 1), ('SQL', 1), ('Verilog', 1), ('VHDL', 1), 
 ('Node.js', 2), ('OpenGL', 2), ('Vulkan', 2), ('DirectX', 2), ('Microsoft SQL Server', 2), ('PostgreSQL', 2), 
-('GPL', 3), ('MIT', 3), ('Apache-2.0', 3), ('BSD-3-Clause', 3), ('BSD-2-Clause', 3), ('Public domain', 3);
+('GNU GPL', 3), ('GNU LGPL', 3), ('MIT', 3), ('Apache-2.0', 3), ('BSD-3-Clause', 3), ('BSD-2-Clause', 3), ('Public domain', 3);
 
 select * from [dbo].[tag_category];
 select * from [dbo].[tag];
