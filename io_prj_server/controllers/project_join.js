77350011741eab94b1e890019482b8cbf307da26
Leon Ożóg <leonozog221@gmail.com>
2024-06-16 16:48:30 +0200

    project join functionality to separate controller

---
+++ io_prj_server/controllers/project_join.js
@@ -0,0 +1,52 @@
+exports.join_project = function (req, res) {
+    var project_id = req.params.id;
+    project_model.get_project(project_id).then(project => {
+        res.render('project_join', { project: project });
+    }).catch(err => {
+        switch (err.message) {
+            case 'project not found':
+                res.status(404);
+                break;
+            default:
+                console.log('Join project error: ', err);
+                res.status(500);
+                break;
+        }
+    });
+};
+
+exports.join_request = function (req, res) {
+    var user = req.user;
+    if (!user || user.is_guest) {
+        res.statusMessage = 'Unauthorized';
+        res.status(401).send();
+        return;
+    }
+    var project_id = req.body.project_id;
+    var message = req.body.message;
+    project_model.join_request(user.user_id, project_id, message).then(() => {
+        res.status(200).send();
+    }).catch(err => {
+        switch (err.message) {
+            case 'message too short':
+                res.status(400).send('Wiadomość jest zbyt krótka!');
+                break;
+            case 'message too long':
+                res.status(400).send('Wiadomość jest zbyt długa!');
+                break;
+            case 'already member':
+                res.status(400).send('Jesteś już członkiem tego projektu!');
+                break;
+            case 'request denied':
+                res.status(400).send('Twoje zgłoszenie zostało odrzucone!');
+                break;
+            case 'request pending':
+                res.status(400).send('Zgłoszenie w trakcie rozpatrywania!');
+                break;
+            default:
+                console.log('Join request error: ', err);
+                res.status(500).send('Błąd serwera.');
+                break;
+        }
+    });
+}
--- io_prj_server/routes/project.js
+++ io_prj_server/routes/project.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var project_controller = require('@controllers/project');
+var project_join_controller = require('@controllers/project_join');
 
 router.get('/id/:id', project_controller.get_project);
 
@@ -10,10 +11,11 @@ router.get('/get_my_projects', project_controller.get_my_project_previews);
 
 router.get('/create', project_controller.project_create);
 
-router.get('/join/:id', project_controller.join_project);
-
-router.post('/join', project_controller.join_request);
-
 router.post('/publish', project_controller.publish);
+
+router.get('/join/:id', project_join_controller.join_project);
+
+router.post('/join', project_join_controller.join_request);
+
 
 module.exports = router;
